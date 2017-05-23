da7a456c3c7bedcd9e9d99c4d164681db4cb5041
Brian Sweeney <bsweeney930@gmail.com>
2017-05-23 17:51:47 -0400

    somewhat stable file here

--- app/js/index.js
+++ app/js/index.js
@@ -16,57 +16,8 @@ import SlopeChart from "./components/SlopeChart/SlopeChart.jsx";
 // add's UA info to html class (needed for iPad, see initPage.js)
 import "./sniff.min.js";
 
-import { loadVizData } from "./viz/loadVizData.js";
-
-loadVizData()
-    .then(json => {
-
-        const { cleanData, locations, indicators, zips } = json;
-
-        const states = R.filter(l => l.abbreviation !== "US", locations);
 
 
-        /* slope chart */
-        ReactDOM.render(
-            <SlopeChart data={{cleanData, locations, indicators}} />,
-            document.querySelector(".js-viz-slope-chart")
-        );
-
-
-        /* location overall */
-        const locationOverallSelector = ".js-viz-location-overall";
-
-        const locationOverall = createLocationOverall({
-            selector: locationOverallSelector,
-            width: $(locationOverallSelector).width(),
-            height: 550,
-            locations: states, indicators, cleanData,
-            onUnlock: function(){
-                // clear the typeahead when the active location is unlocked
-                locationOverallTypeAhead.setValue("");
-            }
-        });
-
-        resize$.onValue(e => {
-            locationOverall.setWidth($(locationOverallSelector).width());
-        });
-
-
-        // location overall typeahead
-        const locationOverallTypeAhead = createTypeAhead(
-            ".js-viz-location-overall__typeahead .typeahead",
-            "name",
-            zipHrrSource(states, zips),
-            function(hrr){
-                locationOverall.setActiveLocation(hrr.id);
-            }
-        );
-
-        // after the viz's are done, force a reflow so the sticky's are correct
-        reflow$.emit("viz's loaded");
-
-
-    });
 
 /** initialize all the page scripts for navigation and sticky vizs, etc **/
 initPage();
